ee86652a822352a41f63d8d44dd8f39133f94ace
ShinEunJin <sineun5501@naver.com>
2021-12-02 23:58:31 +0900

    not db use only storage

--- src/screens/main/Main.js
+++ src/screens/main/Main.js
@@ -1,116 +1,120 @@
-import React, { useState, useEffect } from 'react'
-import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native'
-import { useSelector, useDispatch } from 'react-redux'
-import axios from 'axios'
+import React, {useState, useEffect} from 'react';
+import {Pressable, StyleSheet, Text, TextInput, View} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-import { add, reset } from '../../modules/calculate'
+import {add, reset} from '../../modules/calculate';
 
 const Main = () => {
+  const calculate = useSelector(state => state.calculate);
 
-    const calculate = useSelector(state => state.calculate)
+  const [amount, setAmount] = useState('');
+  const [name, setName] = useState('');
+  const [ledger, setLedger] = useState([]);
 
-    const [amount, setAmount] = useState("")
-    const [name, setName] = useState("")
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch()
+  useEffect(() => {
+    getStorage();
+  }, []);
 
-    useEffect(() => {
-        getStorage()
-    }, [])
-
-    const setStorage = async () => {
-        try {
-            let value = JSON.stringify({ name, amount: Number(amount) })
-            await AsyncStorage.setItem('ledger', value)
-        } catch (error) {
-            console.log(error)
-        }
+  const setStorage = async () => {
+    const list = [];
+    try {
+      let value = {name, amount: Number(amount)};
+      setLedger([...ledger, value]);
+      console.log('ledger', ledger);
+      await AsyncStorage.setItem('ledger', JSON.stringify(ledger));
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const getStorage = async () => {
-        try {
-            let value = await AsyncStorage.getItem('ledger')
-            if (value !== null) {
-                console.log(JSON.parse(value))
-            }
-        } catch (error) {
-            console.log(error)
-        }
+  const getStorage = async () => {
+    try {
+      let value = await AsyncStorage.getItem('ledger');
+      setLedger(JSON.parse(value));
+      if (value !== null) {
+        console.log(JSON.parse(value));
+      }
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    const onAddAmount = async () => {
-        // const { data } = await axios.post("https://shin.loca.lt/api/ledger", { user: "shin-tester3", list: { name: "test", amount: Number(amount) } })
-        dispatch(add(Number(amount)))
-        setAmount("")
-        setName("")
-        setStorage()
-    }
+  const onAddAmount = async () => {
+    // const { data } = await axios.post("https://shin.loca.lt/api/ledger", { user: "shin-tester3", list: { name: "test", amount: Number(amount) } })
+    dispatch(add(Number(amount)));
+    setAmount('');
+    setName('');
+    setStorage();
+  };
 
-    const onResetAmount = () => {
-        dispatch(reset())
-    }
+  const onResetAmount = () => {
+    dispatch(reset());
+  };
 
-    return (
-        <View style={styles.container}>
-            <View>
-                <Text style={styles.titleStyle}>이번 달 지출은?</Text>
-            </View>
-            <View>
-                <Text style={styles.amountStyle}>{calculate.amount}</Text>
-            </View>
-            <TextInput
-                style={styles.inputStyle}
-                keyboardType="numeric"
-                onChangeText={setAmount}
-                value={amount}
-                placeholder="가격"
-                placeholderTextColor="black"
-            />
-            <TextInput
-                style={styles.inputStyle}
-                onChangeText={setName}
-                value={name}
-                placeholder="제목"
-                placeholderTextColor="black"
-            />
-            <Pressable style={styles.inputBtn} onPress={onAddAmount}>
-                <Text style={styles.inputBtnText}>추가</Text>
-            </Pressable>
-            <Pressable style={styles.inputBtn} onPress={onResetAmount}>
-                <Text style={styles.inputBtnText}>초기화</Text>
-            </Pressable>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <View>
+        <Text style={styles.titleStyle}>이번 달 지출은?</Text>
+      </View>
+      <View>
+        <Text style={styles.amountStyle}>{calculate.amount}</Text>
+      </View>
+      <TextInput
+        style={styles.inputStyle}
+        keyboardType="numeric"
+        onChangeText={setAmount}
+        value={amount}
+        placeholder="가격"
+        placeholderTextColor="black"
+      />
+      <TextInput
+        style={styles.inputStyle}
+        onChangeText={setName}
+        value={name}
+        placeholder="제목"
+        placeholderTextColor="black"
+      />
+      <Pressable style={styles.inputBtn} onPress={onAddAmount}>
+        <Text style={styles.inputBtnText}>추가</Text>
+      </Pressable>
+      <Pressable style={styles.inputBtn} onPress={onResetAmount}>
+        <Text style={styles.inputBtnText}>초기화</Text>
+      </Pressable>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff'
-    },
-    titleStyle: {
-        color: '#000'
-    },
-    amountStyle: {
-        color: '#000',
-        fontSize: 24,
-        fontWeight: 'bold'
-    },
-    inputStyle: {
-        borderWidth: 1,
-        borderColor: '#000',
-        color: '#000'
-    },
-    inputBtn: {
-        backgroundColor: '#000',
-        width: 100,
-        alignItems: 'center'
-    },
-    inputBtnText: {
-        color: '#fff',
-        padding: 10
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  titleStyle: {
+    color: '#000',
+  },
+  amountStyle: {
+    color: '#000',
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  inputStyle: {
+    borderWidth: 1,
+    borderColor: '#000',
+    color: '#000',
+  },
+  inputBtn: {
+    backgroundColor: '#000',
+    width: 100,
+    alignItems: 'center',
+  },
+  inputBtnText: {
+    color: '#fff',
+    padding: 10,
+  },
+});
 
-export default Main
+export default Main;
