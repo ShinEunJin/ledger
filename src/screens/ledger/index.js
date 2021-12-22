bc87ce8c47672fe2c4e808a2c24b2f0993deeec9
ShinEunJin <sineun5501@naver.com>
2021-12-22 23:04:27 +0900

    ledger indicator acitivity add

--- src/screens/ledger/index.js
+++ src/screens/ledger/index.js
@@ -1,5 +1,13 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {ImageBackground, Pressable, StyleSheet, Text, View} from 'react-native';
+import {
+  ImageBackground,
+  Pressable,
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Dimensions,
+} from 'react-native';
 import {shallowEqual, useDispatch, useSelector} from 'react-redux';
 import {useFocusEffect, useNavigation} from '@react-navigation/core';
 import dayjs from 'dayjs';
@@ -8,7 +16,6 @@ import Icon_SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 import Icon_MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 import Calendar from '../../components/Calendar';
-import {G_backgroundColor} from '../../global';
 import {setDay} from '../../redux/calendar';
 import {LEDGER_WRITE} from '../../navigations/RootNavigation';
 import storage from '../../storage';
@@ -20,6 +27,7 @@ const Ledger = () => {
 
   const [sum, setSum] = useState(0);
   const [totalSum, setTotalSum] = useState(0);
+  const [loading, setLoading] = useState(true);
 
   const getSum = async () => {
     let data = await storage.getDataByDay(day);
@@ -43,95 +51,110 @@ const Ledger = () => {
 
   useFocusEffect(
     useCallback(() => {
+      setLoading(true);
       getSum(day);
       getMonth(day.substring(5, 7));
-    }, [day]),
+      if (totalSum) setLoading(false);
+    }, [day, totalSum]),
   );
 
   return (
     <ImageBackground
       source={require('../../assets/img/img4.jpg')}
       style={{flex: 1}}>
-      <Calendar />
-      <View style={styles.infoContainer}>
-        <View>
-          <View style={{marginBottom: 5}}>
-            <View style={[styles.itemBox]}>
-              <Icon_AntDesign
-                size={14}
-                name="calendar"
-                color="#000"
-                style={{marginRight: 6}}
-              />
-              <Text style={[styles.textStyle, {fontSize: 14}]}>
-                {dayjs(day).format('YYYY년 MM월 DD일')}
-              </Text>
-            </View>
-          </View>
-          <View style={styles.listBoxStyle}>
-            <View style={[styles.itemBox]}>
-              <Icon_MaterialIcons
-                name="payments"
-                color="#000"
-                size={20}
-                style={{marginRight: 8}}
-              />
-              <Text style={styles.textStyle}>
-                {day.substring(5, 7)}월 달 지출
-              </Text>
-            </View>
-            <View style={{paddingLeft: 20}}>
-              <Text
-                style={[
-                  styles.textStyle,
-                  {fontWeight: '600', color: '#F90716'},
-                ]}>
-                {$numeral(totalSum).format('0,0')
-                  ? $numeral(totalSum).format('0,0')
-                  : 0}
-              </Text>
-            </View>
-          </View>
-          <View style={styles.listBoxStyle}>
-            <View style={[styles.itemBox]}>
-              <Icon_MaterialIcons
-                name="payment"
-                color="#000"
-                size={20}
-                style={{marginRight: 8}}
-              />
-              <Text style={styles.textStyle}>당일 지출</Text>
-            </View>
-            <View style={{paddingLeft: 20}}>
-              <Text
-                style={[
-                  styles.textStyle,
-                  {
-                    fontWeight: '600',
-                    color: !sum || sum === '0' ? '#000' : '#F58840',
-                  },
-                ]}>
-                {$numeral(sum).format('0,0') ? $numeral(sum).format('0,0') : 0}
-              </Text>
-            </View>
-          </View>
-        </View>
-        <Pressable
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            height: 40,
-          }}
-          onPress={() => navigation.navigate(LEDGER_WRITE, {day})}>
-          <Icon_SimpleLineIcons
-            size={16}
-            style={{marginRight: 4}}
-            name="note"
-            color="#000"
+      {loading ? (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator
+            size="large"
+            style={{marginBottom: Dimensions.get('screen').height * 0.15}}
           />
-          <Text style={styles.textStyle}>작성</Text>
-        </Pressable>
-      </View>
+        </View>
+      ) : (
+        <>
+          <Calendar />
+          <View style={styles.infoContainer}>
+            <View>
+              <View style={{marginBottom: 5}}>
+                <View style={[styles.itemBox]}>
+                  <Icon_AntDesign
+                    size={14}
+                    name="calendar"
+                    color="#000"
+                    style={{marginRight: 6}}
+                  />
+                  <Text style={[styles.textStyle, {fontSize: 14}]}>
+                    {dayjs(day).format('YYYY년 MM월 DD일')}
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.listBoxStyle}>
+                <View style={[styles.itemBox]}>
+                  <Icon_MaterialIcons
+                    name="payments"
+                    color="#000"
+                    size={20}
+                    style={{marginRight: 8}}
+                  />
+                  <Text style={styles.textStyle}>
+                    {day.substring(5, 7)}월 달 지출
+                  </Text>
+                </View>
+                <View style={{paddingLeft: 20}}>
+                  <Text
+                    style={[
+                      styles.textStyle,
+                      {fontWeight: '600', color: '#F90716'},
+                    ]}>
+                    {$numeral(totalSum).format('0,0')
+                      ? $numeral(totalSum).format('0,0')
+                      : 0}
+                  </Text>
+                </View>
+              </View>
+              <View style={styles.listBoxStyle}>
+                <View style={[styles.itemBox]}>
+                  <Icon_MaterialIcons
+                    name="payment"
+                    color="#000"
+                    size={20}
+                    style={{marginRight: 8}}
+                  />
+                  <Text style={styles.textStyle}>당일 지출</Text>
+                </View>
+                <View style={{paddingLeft: 20}}>
+                  <Text
+                    style={[
+                      styles.textStyle,
+                      {
+                        fontWeight: '600',
+                        color: !sum || sum === '0' ? '#000' : '#F58840',
+                      },
+                    ]}>
+                    {$numeral(sum).format('0,0')
+                      ? $numeral(sum).format('0,0')
+                      : 0}
+                  </Text>
+                </View>
+              </View>
+            </View>
+            <Pressable
+              style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                height: 40,
+              }}
+              onPress={() => navigation.navigate(LEDGER_WRITE, {day})}>
+              <Icon_SimpleLineIcons
+                size={16}
+                style={{marginRight: 4}}
+                name="note"
+                color="#000"
+              />
+              <Text style={styles.textStyle}>작성</Text>
+            </Pressable>
+          </View>
+        </>
+      )}
     </ImageBackground>
   );
 };
