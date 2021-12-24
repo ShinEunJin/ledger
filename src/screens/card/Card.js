5089efb9133e403d8ea45c36af25c11e645fef0e
ShinEunJin <sineun5501@naver.com>
2021-12-24 23:11:39 +0900

    card test

--- src/screens/card/Card.js
+++ src/screens/card/Card.js
@@ -5,7 +5,37 @@ import axios from 'axios';
 import {useFocusEffect} from '@react-navigation/native';
 
 const Card = () => {
-  useFocusEffect(useCallback(() => {}, []));
+  const getData = async () => {
+    const {data} = await axios.post(
+      'https://developers.nonghyup.com/InquireCreditCardAuthorizationHistory.nh',
+      {
+        Header: {
+          ApiNm: 'InquireCreditCardAuthorizationHistory',
+          Tsymd: '20211224',
+          Trtm: '000755',
+          Iscd: '001387',
+          FintechApsno: '001',
+          ApiSvcCd: 'DrawingTransferA',
+          IsTuno: '3020000006043',
+          AccessToken:
+            'b1c647e9302a0dcff39a0dcd0089cdabca6dd6973dd6f5828d1aa6dbb12c6707',
+        },
+        FinCard: '9411123456782718',
+        IousDsnc: '1',
+        Insymd: '20191105',
+        Ineymd: '20191109',
+        PageNo: '1',
+        Dmcnt: '15',
+      },
+    );
+    console.log({data});
+  };
+
+  useFocusEffect(
+    useCallback(() => {
+      getData();
+    }, []),
+  );
 
   return (
     <View>
